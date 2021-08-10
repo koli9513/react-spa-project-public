38a8ec8f64544ebc83cc31c810b79a5ace803f57
Anita Nagy <anitanagy1989@gmail.com>
2021-08-10 13:12:12 +0200

    feat: add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BookList />
     </div>
   );
