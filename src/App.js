6e16e79765baa303f2789f366f4464e1f7bbda62
Karcub <rokaszemumenyecske@gmail.com>
2021-08-10 17:27:19 +0200

    feat: simple theme switcher (normal/dark)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
-import { useState } from "react";
+import {useState} from "react";
 import { FavoriteContext } from "./components/FavoriteContext";
 import "./App.css";
+import ThemeContext from "./components/ThemeContext";
 
 function App() {
   const [favoriteBooks, setFavoriteBooks] = useState([]);
+  const themeHook = useState("normal");
 
   return (
-    <FavoriteContext.Provider value={{ favoriteBooks, setFavoriteBooks }}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <BookList />
-            </Route>
-            <Route path="/genres">Genres</Route>
-            <Route path="/favorites">Favorites</Route>
-          </Switch>
-        </div>
-      </Router>
-    </FavoriteContext.Provider>
+      <ThemeContext.Provider value={themeHook}>
+        <FavoriteContext.Provider value={{ favoriteBooks, setFavoriteBooks }}>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <Switch>
+                <Route exact path="/">
+                  <BookList />
+                </Route>
+                <Route path="/genres">Genres</Route>
+                <Route path="/favorites">Favorites</Route>
+              </Switch>
+            </div>
+          </Router>
+        </FavoriteContext.Provider>
+      </ThemeContext.Provider>
   );
 }
 
---
+++ src/components/AppTheme.js
@@ -0,0 +1,12 @@
+const AppTheme = {
+  normal: {
+    color: "#372964",
+    backgroundColor: "#9a79dc"
+  },
+  dark: {
+    color: "#23ffa2",
+    backgroundColor: "#093f29"
+  },
+};
+
+export default AppTheme;
