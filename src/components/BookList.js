69716da3c6516fe503ba9bd547cc2d68e249d7a7
Karcub <rokaszemumenyecske@gmail.com>
2021-08-10 13:16:45 +0200

    feat: book list correct missing properties (cover img, publish date)

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,39 +4,49 @@ import BookCard from "./BookCard";
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
+  const url = `https://www.googleapis.com/books/v1/volumes?q="${getRandomLetter()}&maxResults=30`;
+  const getBooks = () => {
+    axios.get(url).then((response) => {
+      const booksFromServer = response.data.items;
+      const cleaned = correctMissingProperties(booksFromServer);
+      setBooks(cleaned);
+    });
+  };
 
   function getRandomLetter() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
   }
 
-  const url = "https://www.googleapis.com/books/v1/volumes?q=" + `${getRandomLetter()}` + "&maxResults=30";
-
-  const getBooks = () => {
-    axios.get(url).then((response) => {
-      const booksFromServer = response.data.items;
-      setBooks(booksFromServer);
-      console.log(booksFromServer)
-      console.log(url)
+  function correctMissingProperties (books) {
+    return books.map((book) => {
+      if (book.volumeInfo.hasOwnProperty('publishedDate') === false) {
+        book.volumeInfo['publishedDate'] = '0000';
+      } else if (book.volumeInfo.hasOwnProperty('imageLinks') === false) {
+        book.volumeInfo['imageLinks'] = {
+          cover: 'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg'
+        }
+      }
+      return book;
     });
-  };
+  }
 
   useEffect(() => {
     getBooks();
   }, []);
 
   return (
-    <div className="book list">
-      {books.map((book, index) => (
-        <BookCard
-          key={index}
-          cover={book.volumeInfo.imageLinks.smallThumbnail}
-          author={book.volumeInfo.authors}
-          title={book.volumeInfo.title}
-          published={book.volumeInfo.publishedDate}
-        />
-      ))}
-    </div>
+      <div className="book list">
+        {books.map((book, index) => (
+            <BookCard
+                key={index}
+                cover={book.volumeInfo.imageLinks.smallThumbnail}
+                author={book.volumeInfo.authors}
+                title={book.volumeInfo.title}
+                published={book.volumeInfo.publishedDate}
+            />
+        ))}
+      </div>
   );
 };
 
