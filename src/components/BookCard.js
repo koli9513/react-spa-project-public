796d1943fa50465dd3281769fe3053817f626e7b
Karcub <rokaszemumenyecske@gmail.com>
2021-08-10 13:22:38 +0200

    feat: book card basic style

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,14 +2,14 @@ import {BookCardStyle} from "./BookCardStyle";
 
 const BookCard = (props) => {
   return (
-      <div>
+      <BookCardStyle>
         <img src={props.cover} alt="cover"/>
           <div className="book info">
               <h5>{props.title}</h5>
               <h6>{props.author}</h6>
               <p>{props.published}</p>
           </div>
-      </div>
+      </BookCardStyle>
   )
 };
 
