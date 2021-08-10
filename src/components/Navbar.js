eb1a0d9036f6ff92a6dd47970b694f73ff34e0a9
Anita Nagy <anitanagy1989@gmail.com>
2021-08-10 13:27:31 +0200

    feat: add Navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>Search</li>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link to="/genres">Genres</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
