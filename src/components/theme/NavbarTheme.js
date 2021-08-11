755098f8cf77918529e2f0d1167b1734ea273d08
Karcub <rokaszemumenyecske@gmail.com>
2021-08-11 19:45:02 +0200

    feat: added color theme for search in navbar

--- src/components/theme/NavbarTheme.js
+++ src/components/theme/NavbarTheme.js
@@ -3,11 +3,13 @@ const NavbarTheme = {
         backgroundColor: "#31e6a5",
         color: "#00885a",
         borderColor: "#251d44",
+        lowerBackgroundColor: "#8afcce"
     },
     dark: {
         backgroundColor: "#251d44",
         color: "#9054cf",
         borderColor: "#31e6a5",
+        lowerBackgroundColor: "#342c70"
     },
 };
 
