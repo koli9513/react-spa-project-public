ad9c39334b31b8f5a8fc9d8d196f5e196c273ea2
Karcub <rokaszemumenyecske@gmail.com>
2021-08-11 20:06:06 +0200

    feat: added button theme for detailed view button on card

--- src/components/styles/BookCardContainerStyle.js
+++ src/components/styles/BookCardContainerStyle.js
@@ -3,9 +3,10 @@ import styled, {} from "styled-components";
 export const BookCardsContainerStyle = styled.div
     .attrs({className: 'cards'})`
   padding: 100px 50px;
+  border-radius: 5px;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  grid-column-gap: 10px;
+  grid-column-gap: 20px;
   grid-row-gap: 10px;
   min-height: 537px;
 `;
