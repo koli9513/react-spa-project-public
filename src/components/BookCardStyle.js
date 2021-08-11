3166701b7df57d5c652f8f9f5738950305fd7640
Karcub <rokaszemumenyecske@gmail.com>
2021-08-11 15:29:16 +0200

    feat: added box shadow hover effect to book card

--- src/components/BookCardStyle.js
+++ src/components/BookCardStyle.js
@@ -2,10 +2,17 @@ import styled, {css} from "styled-components";
 
 export const BookCardStyle = styled.div
     .attrs({className: 'card'})`
+  position: relative;
+  
   > .image-left {
-    //width: 15%;
     float: left;
     height: 280px;
     min-width: 190px;
   }
+
+    ${props => props && css`
+    &:hover {
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+  `}
 `;
