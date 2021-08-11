c99d290a769bb2b66b02c2a853f5794449ea3345
Karcub <rokaszemumenyecske@gmail.com>
2021-08-11 19:46:48 +0200

    fix: moved to style directory

--- src/components/styles/BookCardContainerStyle.js
+++ src/components/styles/BookCardContainerStyle.js
@@ -2,9 +2,10 @@ import styled, {} from "styled-components";
 
 export const BookCardsContainerStyle = styled.div
     .attrs({className: 'cards'})`
-  padding: 30px 50px;
+  padding: 100px 50px;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   grid-column-gap: 10px;
   grid-row-gap: 10px;
+  min-height: 537px;
 `;
