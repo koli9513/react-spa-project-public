41c3ea48b195608b13f5ccf6a4a07d15a0a857c7
koli9513 <66993009+koli9513@users.noreply.github.com>
2021-08-12 15:14:14 +0200

    fix: add error handling for axios fetch

--- src/components/helpers/useFetch.js
+++ src/components/helpers/useFetch.js
@@ -31,7 +31,21 @@ export default function useFetch(url) {
         const booksFromServer = response.data.items;
         const cleaned = correctMissingProperties(booksFromServer);
         setBooks(cleaned);
-      });
+      })
+          .catch((error) => {
+
+            if (error.response) {
+               console.log(error.response.data);
+                console.log(error.response.status);
+               console.log(error.response.headers);
+            } else if (error.request) {
+
+              console.log(error.request);
+            } else {
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          });
     };
     getBooks();
   }, [url]);
