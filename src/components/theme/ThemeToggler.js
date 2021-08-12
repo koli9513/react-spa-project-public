422224adabdec25192b298cbff819056470af2b9
Anita Nagy <anitanagy1989@gmail.com>
2021-08-12 17:00:57 +0200

    feat: theme saved in local storage

--- src/components/theme/ThemeToggler.js
+++ src/components/theme/ThemeToggler.js
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import ThemeContext from "../contexts/ThemeContext";
 import styled from "styled-components";
 
-const ThemeTogglerStyle = styled.button
-    .attrs({className: 'switch'})`
+const ThemeTogglerStyle = styled.button.attrs({ className: "switch" })`
   //margin-left: 1450px;
   background-color: inherit;
   width: 40px;
@@ -13,10 +12,14 @@ const ThemeTogglerStyle = styled.button
 const ThemeToggler = () => {
   const [themeMode, setThemeMode] = useContext(ThemeContext);
 
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(themeMode));
+  }, [themeMode]);
+
   return (
     <ThemeTogglerStyle
-        className="switch"
-        onClick={() => {
+      className="switch"
+      onClick={() => {
         setThemeMode(themeMode === "normal" ? "dark" : "normal");
       }}
     >
