35969d20e8c0966e6215b9530c8ad890277fb825
Anita Nagy <anitanagy1989@gmail.com>
2021-08-12 13:43:42 +0200

    feat: add BrowseBookList to Route in App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,50 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BookList from "../lists/BookList";
 import Navbar from "../navbar/Navbar";
-import {useState} from "react";
+import { useState } from "react";
 import { FavoriteContext } from "../contexts/FavoriteContext";
 import "./App.css";
 import FavoriteList from "../lists/FavoriteList";
 import ThemeContext from "../contexts/ThemeContext";
 import BookDetailedView from "../detailedview/BookDetailedView";
 import SearchList from "../lists/SearchList";
-
+import BrowseBookList from "../lists/BrowseBookList";
 
 function App() {
   const [favoriteBooks, setFavoriteBooks] = useState([]);
   const themeHook = useState("normal");
 
   return (
-      <ThemeContext.Provider value={themeHook}>
-        <FavoriteContext.Provider value={{ favoriteBooks, setFavoriteBooks }}>
-          <Router>
-            <div className="App">
-              <Navbar />
-              <Switch>
-                <Route exact path="/"><BookList /></Route>
-                <Route path="/browse"><BookList /></Route>
-                <Route path="/search/:searchType/:searchTerm"><SearchList/></Route>
-                <Route path="/advanced/:searchAuthor/:searchTitle"><SearchList/></Route>
-                <Route path="/genres">Genres</Route>
-                <Route path="/favorites"><FavoriteList /></Route>
-                <Route path="/book/:bookId"><BookDetailedView /></Route>
-              </Switch>
-            </div>
-          </Router>
-        </FavoriteContext.Provider>
-      </ThemeContext.Provider>
+    <ThemeContext.Provider value={themeHook}>
+      <FavoriteContext.Provider value={{ favoriteBooks, setFavoriteBooks }}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                <BookList />
+              </Route>
+              <Route path="/browse">
+                <BrowseBookList />
+              </Route>
+              <Route path="/search/:searchType/:searchTerm">
+                <SearchList />
+              </Route>
+              <Route path="/advanced/:searchAuthor/:searchTitle">
+                <SearchList />
+              </Route>
+              <Route path="/genres">Genres</Route>
+              <Route path="/favorites">
+                <FavoriteList />
+              </Route>
+              <Route path="/book/:bookId">
+                <BookDetailedView />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </FavoriteContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import ThemeContext from "../contexts/ThemeContext";
-import {useContext} from "react";
+import { useContext } from "react";
 import NavbarTheme from "../theme/NavbarTheme";
-import {StyledNavbar} from "../styles/StyledNavbar";
+import { StyledNavbar } from "../styles/StyledNavbar";
 import ThemeToggler from "../theme/ThemeToggler";
 import SearchBox from "../search/SearchBox";
-import {StyledNavbarLink} from "../styles/StyledNavbarLink";
-import {StyledSearchBox} from "../styles/StyledSearchBox";
+import { StyledNavbarLink } from "../styles/StyledNavbarLink";
+import { StyledSearchBox } from "../styles/StyledSearchBox";
 import ButtonTheme from "../theme/ButtonTheme";
 import CatGif from "../theme/CatGif";
 
@@ -15,42 +15,52 @@ const Navbar = () => {
   const navbarTheme = NavbarTheme[theme];
   const buttonTheme = ButtonTheme[theme];
   const navLinkContainerStyle = {
-      float: "left",
-      left: "17%",
-      position: "absolute"
-  }
+    float: "left",
+    left: "17%",
+    position: "absolute",
+  };
   const navLinkStyle = {
-      backgroundColor: `${buttonTheme.backgroundColor}`,
-      color: `${buttonTheme.color}`,
-      borderColor: `${buttonTheme.borderColor}`
-  }
+    backgroundColor: `${buttonTheme.backgroundColor}`,
+    color: `${buttonTheme.color}`,
+    borderColor: `${buttonTheme.borderColor}`,
+  };
 
   return (
-      <div>
-          <StyledNavbar style={{
-            backgroundColor: `${navbarTheme.backgroundColor}`,
-            color: `${navbarTheme.color}`,
-            border: `${navbarTheme.borderColor}`
-          }}>
-            <ThemeToggler />
-              <div style={navLinkContainerStyle}>
-                <StyledNavbarLink to="/" style={navLinkStyle}>Home</StyledNavbarLink>
-                {/*<StyledNavbarLink to="/browse" style={navLinkStyle}>Browse books</StyledNavbarLink>*/}
-                <StyledNavbarLink style={navLinkStyle} to="/favorites">Favorites</StyledNavbarLink>
-                <StyledNavbarLink style={navLinkStyle} to="/genres">Genres</StyledNavbarLink>
-              </div>
-              <CatGif cat="1"/>
-              <CatGif cat="2"/>
-          </StyledNavbar>
-          <StyledSearchBox style={{
-              backgroundColor: `${navbarTheme.backgroundColor}`,
-              color: `${navbarTheme.color}`,
-
-          }}>
-              <SearchBox />
-
-          </StyledSearchBox>
-      </div>
+    <div>
+      <StyledNavbar
+        style={{
+          backgroundColor: `${navbarTheme.backgroundColor}`,
+          color: `${navbarTheme.color}`,
+          border: `${navbarTheme.borderColor}`,
+        }}
+      >
+        <ThemeToggler />
+        <div style={navLinkContainerStyle}>
+          <StyledNavbarLink to="/" style={navLinkStyle}>
+            Home
+          </StyledNavbarLink>
+          <StyledNavbarLink to="/browse" style={navLinkStyle}>
+            Browse books
+          </StyledNavbarLink>
+          <StyledNavbarLink style={navLinkStyle} to="/favorites">
+            Favorites
+          </StyledNavbarLink>
+          <StyledNavbarLink style={navLinkStyle} to="/genres">
+            Genres
+          </StyledNavbarLink>
+        </div>
+        <CatGif cat="1" />
+        <CatGif cat="2" />
+      </StyledNavbar>
+      <StyledSearchBox
+        style={{
+          backgroundColor: `${navbarTheme.backgroundColor}`,
+          color: `${navbarTheme.color}`,
+        }}
+      >
+        <SearchBox />
+      </StyledSearchBox>
+    </div>
   );
 };
 
