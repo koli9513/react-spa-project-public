bf7401297f141b323a31e438106b4f4de65a8be5
Karcub <rokaszemumenyecske@gmail.com>
2021-08-12 16:50:27 +0200

    refactor: KeywordProvider.js gets word list from globals

--- src/components/helpers/KeywordProvider.js
+++ src/components/helpers/KeywordProvider.js
@@ -1,3 +1,16 @@
-import Globals from "./Globals";
+const words = [
+    "+inauthor:sigmund+freud",
+    "ShakesPeare",
+    "+inauthor:endre+ady",
+    "+inauthor:jane+austen",
+    "+intitle:qing",
+    "+inauthor:patrick+rothfuss",
+    "+inauthor:sylvia+plath",
+    "+inauthor:j+d+salinger",
+    "+inauthor:tennessee+williams",
+    "+inauthor:j+r+r+tolkien",
+    "+inauthor:terry+pratchett",
+    // "+inauthor:fable+vavyan"
+]
 
-export const KeywordProvider = Globals.words[Math.floor(Math.random()*Globals.words.length)];
+export const KeywordProvider = words[Math.floor(Math.random()*words.length)];
