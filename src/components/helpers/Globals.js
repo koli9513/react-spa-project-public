03d84f1d92baf04e328ad656f7d1ea2df4b3f192
Karcub <rokaszemumenyecske@gmail.com>
2021-08-12 12:32:29 +0200

    refactor: api url extracted to globals

--- src/components/helpers/Globals.js
+++ src/components/helpers/Globals.js
@@ -3,7 +3,9 @@ import {KeywordProvider} from "./KeywordProvider";
 const Globals = {
     notAvailableMessage: <i>Not available</i>,
     missingImgUrl: "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg",
-    homePageApiUrl: `https://www.googleapis.com/books/v1/volumes?q=${KeywordProvider}&maxResults=30`,
+    homePageApiUrl: `https://www.googleapis.com/books/v1/volumes?q=${KeywordProvider}&maxResults=20`,
+    maxResults: "&maxResults=20",
+    apiUrlBase: "https://www.googleapis.com/books/v1/volumes?q="
 };
 
 export default Globals;
